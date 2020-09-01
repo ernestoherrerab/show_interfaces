218cd1089244e5794459bd6385eded232dacecd2
Ernesto <ernesto.herrerab@gmail.com>
2020-09-01 18:25:51 +0200

    Import CSV

--- ans_import_csv.py
+++ ans_import_csv.py
@@ -18,22 +18,22 @@ def read_csv(file):
 
 def create_inventory(hosts_data):
     """Creates the inventory file"""
-    path_file = "hosts"
+    path_file = "hosts_stage.yml"
     with open(path_file, "w") as open_file:
         hosts_file = open_file.write(hosts_data)
     return hosts_file
 
 
-def format_hosts():
-    """Formats the inventory"""
-    path_file = "hosts"
-    host_file = open(path_file)
-    lines = host_file.readlines()
-    host_file.close()
-    host_file = open(path_file, "w")
-    for line in lines:
-        host_file.write(line[1:])
-    host_file.close()
+def clean_yaml_file(input_file, output_file):
+    """Clean Yaml files"""
+    with open(input_file, 'r') as infile, \
+         open(output_file, 'w') as outfile:
+        data = infile.read()
+        to_clean = ["''"]
+        for data_elements in to_clean:
+            data = data.replace(data_elements, "")
+        outfile.write(data)
+    #Path.unlink(input_file)
 
 
 def main():
@@ -44,19 +44,27 @@ def main():
     csv_path_file = csv_folder / "Mgmt_IPs.csv"
     csv_data = read_csv(csv_path_file)
     empty_host = {}
-    host_list_dict = []
+    empty_host['all'] = {}
+    empty_host['all']['vars'] = {}
+    empty_host['all']['vars']['ansible_python_interpreter'] = '/usr/bin/python3'
+    empty_host['all']['children'] = {}   
     for host_list in csv_data:
-        empty_host[host_list[0]] = {
-            "hostname": host_list[0],
-            "groups": [host_list[2] + "_devices"],
-        }
-        host_list_dict.append(empty_host)
-        empty_host = {}
-    hosts_yaml = yaml.dump(host_list_dict, default_flow_style=False)
-    host_yaml_init = "----\n\n" + hosts_yaml
-    create_inventory(str(host_yaml_init))
-    format_hosts()
-
+        if host_list[2] == 'ios':
+            if 'ios' in empty_host['all']['children']:
+                empty_host['all']['children']['ios']['hosts'][host_list[0]]=''
+            else:
+                empty_host['all']['children']['ios'] = {}
+                empty_host['all']['children']['ios']['hosts'] = {}
+                empty_host['all']['children']['ios']['hosts'][host_list[0]] = ''
+                empty_host['all']['children']['ios']['vars'] = {}
+                empty_host['all']['children']['ios']['vars']['ansible_connection'] = 'network_cli'
+                empty_host['all']['children']['ios']['vars']['ansible_network_os'] = 'ios'
+                empty_host['all']['children']['ios']['vars']['ansible_user'] = "{{username}}"
+                empty_host['all']['children']['ios']['vars']['ansible_ssh_pass'] = "{{password}}"    
+    hosts_yaml = yaml.dump(empty_host, default_flow_style=False)
+    create_inventory(str(hosts_yaml))
+    clean_yaml_file('hosts_stage.yml', 'hosts.yml')
+    
 
 if __name__ == "__main__":
     main()
